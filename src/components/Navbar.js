08eef57421ae88c40c4c8e58d2b37839a27f656d
anwachuku15 <anwachuku15@gmail.com>
2019-12-16 14:15:56 -0500

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
+
+// Material-UI
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <AppBar>
+        <Toolbar className='nav-container'>
+          <Button color="inherit" component={Link} to='/'>Home</Button>
+          <Button color="inherit" component={Link} to='/login'>Login</Button>
+          <Button color="inherit" component={Link} to='/signup'>Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+}
+
+export default Navbar
