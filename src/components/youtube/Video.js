e0a25248af220b5d027b296e65690e0b55b35711
anwachuku15 <anwachuku15@gmail.com>
2020-10-12 13:08:52 -0400

    conditional rendering for video title

--- src/components/youtube/Video.js
+++ src/components/youtube/Video.js
@@ -4,10 +4,22 @@ import Container from '@material-ui/core/Container';
 
 
 const Video = ({video, clickVideo}) => {
+    const string = video.snippet.title
+    const substring = '&#39;'
+    // console.log(string)
+    var x = "'"
+    const newString = string.replace(substring, x)
+    const videoTitle = string.includes(substring) ? newString : video.snippet.title
+    console.log(videoTitle)
+    // const substring = '&#39;'
+    // var x = "\""
+    // const newString = string.replace(substring, x)
+    // console.log(newString)
+    // const videoTitle = string.includes(substring) ? newString : video.snippet.title
     return (
-        <Container className='col-sm-8'>
+        <Container>
             <div onClick={() => clickVideo(video)}>
-                <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
+                <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} style={{padding: '0px 0px', maxWidth:'100%', height:'auto'}}/>
             
                 <div>
                     <div style={{color:'#06d4cd'}}>{video.snippet.title}</div>
